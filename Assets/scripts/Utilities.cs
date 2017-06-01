110ca2914c0b376ad7ca7afa0bb37662605e624a
Justin DiMichele <justindimichele@gmail.com>
2017-06-01 16:38:54 -0400

    justin init commit

---
+++ Assets/scripts/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities {
+
+    public static bool isObjectOnScreen(Transform transform)
+    {
+        bool res = false;
+
+        Vector2 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+        res = (screenPoint.x > 0 && screenPoint.x < 1) && (screenPoint.y < 1 && screenPoint.y > 0);
+
+        return res;
+    }
+    
+}
